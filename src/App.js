478b80db7ec96148b635c722fe92ca4671cec349
lioba00700 <lioba00700@naver.com>
2024-06-10 10:32:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './woodstock.png'
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          2학년 1반 8번 박민서
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          프론트엔드 개발자
         </a>
       </header>
     </div>
